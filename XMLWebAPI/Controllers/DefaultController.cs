93d9d0308e6c7b76d3db64bed2e57fc7381975f6
renan campos <renan.campos@live.com>
2019-12-03 17:28:07 +0000

    WebAPI

---
+++ XMLWebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using XMLStructure.InputDocumentStructure;
+
+namespace XMLWebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public int Default(InputDocument pInputDocument)
+        {
+            if (pInputDocument == null || pInputDocument.DeclarationList.Declaration.Command != "DEFAULT")
+                return -1;
+
+            if (pInputDocument == null || pInputDocument.DeclarationList.Declaration.DeclarationHeader.SiteID != "DUB")
+                return -2;
+
+            return 0;
+        }
+    }
+}
